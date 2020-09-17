a3c0a6998070b72200c156f749a62ed23808578d
Ceasar6 <vietanh25696@gmail.com>
2020-09-17 23:48:32 +0700

    'fix-import'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import {LoginComponent} from './customers/login/login.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {ListComponent} from './houses/list/list.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './layouts/home/home.component';
+import {RegisterComponent} from './customers/register/register.component';
+import {AppRoutingModule} from './app-routing.module';
+import {NavbarComponent} from './layouts/navbar/navbar.component';
 
 // @ts-ignore
 @NgModule({
