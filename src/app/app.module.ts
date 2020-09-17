390b04a521435fa11241c5b32b89712016858078
quangbui205 <bmq205999@gmail.com>
2020-09-17 16:31:35 +0700

    create form register customers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './layouts/home/home.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { RegisterComponent } from './customers/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
